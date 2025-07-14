5738abf594c00bb9d8fc1630f37e65cf434827b7
Amarnath WebDev <amarnathkumar2021@gmail.com>
2025-07-14 10:41:15 +0530

    2024-08-24T10:40:04+05:30

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
+import random from "random";
 
 const path = "./data.json";
-const date = moment().subtract(1, 'd').format();
+const git = simpleGit(); // reuse the git instance
 
-const data = {
-  date: date,
+const markCommit = async (n) => {
+  if (n === 0) {
+    await git.push();
+    console.log("✅ Push complete!");
+    return;
+  }
+
+  const x = random.int(0, 54); // random week offset
+  const y = random.int(0, 6);  // random day offset
+
+  const date = moment()
+    .subtract(1, "y")   // go back 1 year
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = { date };
+  console.log("📅 Commit Date:", date);
+
+  jsonfile.writeFile(path, data, async () => {
+    await git.add(path);
+    await git.commit(date, undefined, { "--date": date });
+    markCommit(n - 1);
+  });
 };
 
-jsonfile.writeFile(path, data, ()=>{
-
-    simpleGit().add([path]).commit(date, {'--date': date}).push();
-});
-
-
+markCommit(100);
